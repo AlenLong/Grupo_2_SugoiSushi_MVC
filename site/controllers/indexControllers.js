691092f872b04c8a3ff509880cbfc01caf1da06e
AlenLong <106123213+AlenLong@users.noreply.github.com>
2022-08-11 11:37:24 -0300

    enrutado de todas las vistas

--- site/controllers/indexControllers.js
+++ site/controllers/indexControllers.js
@@ -3,6 +3,23 @@ module.exports = {
     index: (req,res)=>{
         return res.render('index')
     },
+
+    detail: (req,res)=>{
+        return res.render('detail')
+    },
+
+    carrito: (req,res)=>{
+        return res.render('carrito')
+    },
+
+    register: (req,res)=>{
+        return res.render('register')
+    },
+
+    login: (req,res)=>{
+        return res.render('login')
+    },
+
     nosotros: (req,res)=>{
         return res.render('nosotros')
     },
