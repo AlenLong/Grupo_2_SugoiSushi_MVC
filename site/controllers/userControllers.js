021713c19f720246b6cd08b4820509add90f8931
AlenLong <106123213+AlenLong@users.noreply.github.com>
2022-09-14 23:08:51 -0300

    estructura login, comparacion de password y roll

--- site/controllers/userControllers.js
+++ site/controllers/userControllers.js
@@ -23,14 +23,47 @@ module.exports = {
         guardar(users)
     
         res.redirect('/users/login')
-        /* console.log(req.body)
-        return res.json(req.body) */
+
     },
-
-
 
     
     login: (req,res)=>{
         return res.render('./users/login')
+    },
+    loginPost: (req,res)=>{
+        let {email, password} = req.body
+        /*         let userLogin = {}
+                //comparar el email ingresado, con user.email, si coincide, repetir con la pass
+                users.forEach(function(user) {
+                    console.log(user.email)
+                    if (email == user.email){
+                        userLogin = user
+                    } else {
+                        console.log("El usuario no existe") //ver manejo de errores
+                        res.json(req.body)
+                        //res.redirect('/users/login')
+                    } 
+                }) */
+        let userLogin = users.filter(function(user) {
+            return user.email === email
+        })
+
+        // filter devuelve un array, si el array esta vacio el usuario no existe
+        if (!userLogin.length) {
+            console.log("El usuario no existe") //ver manejo de errores
+            return res.redirect('/users/login')
+        }
+
+        // si el usuario existe, comparo y valido su password y roll
+        if (password === userLogin[0].password){
+            if (userLogin[0].roll === "admin"){
+                return res.redirect('/admin/listarProducts')
+            } else{
+            res.redirect('/carrito')
+            }
+        } else {
+            console.log('password incorrecta') //chequear manejo de errores
+            return res.redirect('/users/login')
+        }
     },
 }
--- site/routes/usersRoutes.js
+++ site/routes/usersRoutes.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 
-let {login,register,registerPost} = require('../controllers/userControllers')
+let {login,register,registerPost,loginPost} = require('../controllers/userControllers')
 
 router.get('/register',register)
 router.post('/register',registerPost)
 router.get('/login',login)
-router.post('/login',login)
+router.post('/login',loginPost)
 
 module.exports = router
