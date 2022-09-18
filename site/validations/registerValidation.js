2c73c3549a26170bde11db4843c3a9f93eb2168a
AlenLong <106123213+AlenLong@users.noreply.github.com>
2022-09-17 22:49:27 -0300

    se editaron los mensajes de error en el registro

--- site/validations/registerValidation.js
+++ site/validations/registerValidation.js
@@ -1,11 +1,17 @@
 const {check} = require('express-validator');
 
 module.exports = [
-    check('nombre').notEmpty().withMessage("Campo obligatorio"),
-    check('apellido').notEmpty().withMessage("Campo obligatorio"),
-    check('email').notEmpty().withMessage("Campo obligatorio")
-    .bail().isLength({min:5}).withMessage('debe incluir al menos 5 caracteres'),
-    check('password').notEmpty().withMessage("Campo obligatorio"),
-    check('confirma').notEmpty().withMessage("Campo obligatorio")
+    check('nombre').trim().notEmpty().withMessage("Debe ingresar su nombre")
+    .bail().isLength({min:2}).withMessage('Debe contener al menos 2 caracteres'),
+
+    check('apellido').trim().notEmpty().withMessage("Debe ingresar su apellido")
+    .bail().isLength({min:2}).withMessage('Debe contener al menos 2 caracteres'),
+
+    check('email').trim().notEmpty().withMessage("Debe ingresar su email")
+    .bail().isEmail().withMessage('Debe ingresar un email valido'),
+
+    check('password').isLength({min:8}).withMessage('Debe contener al menos 8 caracteres'),
+    
+    check('confirma').isLength({min:8}).withMessage('Debe contener al menos 8 caracteres')
     /* check('imagen').notEmpty().withMessage("Campo obligatorio") */ // express solo valida strings, crear custom validator y enlazarlo con el de multer
 ]
