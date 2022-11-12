8283b1ad6f7ba26a8663cfbd8d102ee0fd6a06b9
Joaquin Leguizamon <joaquinleguizamon03@gmail.com>
2022-11-11 23:08:30 -0300

    Subo validaciones front y sweet alert

--- site/validations/productsValidation.js
+++ site/validations/productsValidation.js
@@ -1,26 +1,37 @@
 const {check} = require('express-validator');
 
 module.exports = [
+    
     /* Categoria */
+    
     check('categoriasId').trim().notEmpty().
-    withMessage('Este campo es obligatorio'),
+    withMessage('Debe seleccionar una categoria'),
+    
     /* Nombre */
+    
     check('nombreProducto').trim().notEmpty()
-    .withMessage('Este campo es obligatorio'),
+    .withMessage('Debe darle un nombre al producto'),
+    
     /* Descripcion */
+    
     check('descripcion').trim()
-    .notEmpty().withMessage('Este campo es obligatorio').bail().isLength({min: 10}).withMessage('Minimo 10 carácteres'),
+    .notEmpty().withMessage('Describa su producto').bail().isLength({min: 10}).withMessage('Minimo 10 carácteres'),
+    
     /* Precio */
+    
     check('precio').trim().notEmpty()
     .withMessage('Este campo es obligatorio').bail()
     .isInt()
     .withMessage('Solo carácteres numericos'),
+    
     /* Descuento */
+    
     check('descuento').trim()
     .isInt()
     .withMessage('Solo carácteres numericos'),
-    /* Stock */
+    
+    /* Disponibilidad */
+    
     check('disponible').trim().notEmpty()
-    .withMessage('Este campo es obligatorio')
-    .isInt(),
+    .withMessage('Este campo es obligatorio'),
 ]
