bf3f33276059500082659ce2b122b95c563f69e4
JoseLC07 <joseloly7@gmail.com>
2022-12-15 09:21:40 -0300

    nuevo

--- site/controllers/indexControllers.js
+++ site/controllers/indexControllers.js
@@ -1,9 +1,36 @@
 const db = require("../database/models");
 const productos = require("../database/models/productos");
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 module.exports = {
     index: (req,res)=>{
-        db.Productos.findAll({
+        let productoDescuentos = db.Productos.findAll({
+            limit: 3,
+            order: [["descuento", "desc"]],
+        })
+        let productosNuevos = db.Productos.findAll({
+            limit:3,
+            order: [["createdAt", "desc"]],
+        })
+        let productosVeggie = db.Productos.findAll({
+            limit:3,
+            where: {categoriasId: 1}
+        })
+        
+        Promise.all([productoDescuentos,productosNuevos,productosVeggie])
+        .then(([productoDescuentos,productosNuevos,productosVeggie]) => {
+           /* return res.send(productosVeggie)   */   
+                return res.render('index', {
+                    productoDescuentos,
+                    productosNuevos,
+                    productosVeggie,
+                    toThousand
+
+                })
+        })
+        .catch(error => res.send(error))
+
+      /*   db.Productos.findAll({
             include:[{
                 all:true
             }]
@@ -12,6 +39,7 @@ module.exports = {
             db.Productos.findAll({
                 where: {
                     categoriasId: producto.categoriasId
+
                 },
                 limit: 3,
                 order: [[Sequelize.literal("RAND()")]],
@@ -21,14 +49,14 @@ module.exports = {
             })
                 .then(productos => {
                     /* return res.send(productos) */
-                    return res.render('index', {
+                /*     return res.render('index', {
                         producto,
                         productos
                     })
                 })
         })
         .catch(error => console.log(error))
-        return res.render('index')
+        return res.render('index')  */
     },
 
     detail: (req,res)=>{
