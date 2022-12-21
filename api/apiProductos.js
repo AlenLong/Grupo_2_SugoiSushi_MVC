1a9c60a4d9db79d4cbd7838cff43f9aa81007873
abelRmolina <abel.thomas.rodriguez@gmail.com>
2022-12-21 18:06:56 -0300

    reparacion de elemento producto en carrito arreglado

--- api/apiProductos.js
+++
@@ -1,8 +0,0 @@
-const {paginacion} = require('../../controllers/api/paginacion');
-const express = require('express');
-const router = express.Router();
-
-/* GET home page. */
-router.get('/products', paginacion);
-
-module.exports = router;
