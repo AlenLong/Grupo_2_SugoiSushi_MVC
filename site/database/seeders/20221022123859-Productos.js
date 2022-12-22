0844a9db034ed2e9830dd010366dd920504ce2ba
AlenLong <alenlong1@gmail.com>
2022-12-22 20:02:03 -0300

    base de datos img productos

--- site/database/seeders/20221022123859-Productos.js
+++ site/database/seeders/20221022123859-Productos.js
@@ -1,84 +1,21 @@
 'use strict';
 
-let productos = [
-  {
-    nombreProducto: "Bubba",
-    categoriasId: "2",
-<<<<<<< HEAD
-    imagen: "cali.jpg",
-=======
-    imagen: "bubba.jpg",
->>>>>>> d7407497faedefb387e8b5fef8c35be375e48bda
-    descripcion: "neque libero convallis eget eleifend luctus ultricies eu nibh quisque id justo sit amet sapien dignissim vestibulum vestibulum ante",
-    precio: 3595,
-    descuento: 24,
-    createdAt : new Date,
-    updatedAt : new Date
-},
-{
-  nombreProducto: "Langostinos en Panko",
-  categoriasId: "1",
-  disponible: false,
-  imagen: "langos.jpg",
-  descripcion: "nunc purus phasellus in felis donec semper sapien a libero nam dui proin leo odio porttitor id consequat in consequat ut nulla",
-  precio: 1563,
-  descuento: 45,
-  createdAt : new Date,
-  updatedAt : new Date
-},
-{
-  nombreProducto: "New York Phila Hot",
-  categoriasId: "3",
-  disponible: true,
-<<<<<<< HEAD
-  imagen: "nyphot.jpg",
-=======
-  imagen: "new york phila hot png.png",
->>>>>>> d7407497faedefb387e8b5fef8c35be375e48bda
-  descripcion: "nulla ultrices aliquet maecenas leo odio condimentum id luctus nec molestie sed justo pellentesque viverra pede",
-  precio: 1904,
-  descuento: 30,
-  createdAt : new Date,
-  updatedAt : new Date
-},
-{
-  nombreProducto: "Veggie Friendly",
-  categoriasId: "4",
-  disponible: false,
-  imagen: "veggiefriendly.jpg",
-  descripcion: "lacus morbi sem mauris laoreet ut rhoncus aliquet pulvinar sed nisl nunc rhoncus dui vel sem sed sagittis nam congue risus semper porta",
-  precio: 6916,
-  descuento: 30,
-  createdAt : new Date,
-  updatedAt : new Date
-},
-{
-  nombreProducto: "SalsaGodzilla",
-  categoriasId: "5",
-  disponible: false,
-<<<<<<< HEAD
-  imagen: "combi5.jpg",
-=======
-  imagen: "combi1.jpg",
->>>>>>> d7407497faedefb387e8b5fef8c35be375e48bda
-  descripcion: "vitae mattis nibh ligula nec sem duis aliquam convallis nunc proin at turpis a pede posuere nonummy integer non velit donec diam neque",
-  precio: 1293,
-  descuento: 27,
-  createdAt : new Date,
-  updatedAt : new Date
-},
-{
-  nombreProducto: "Bubba",
-  categoriasId: "3",
-  disponible: true,
-  imagen: "bubba.jpg",
-  descripcion: "turpis a pede posuere nonummy integer non velit donec diam neque vestibulum eget vulputate ut ultrices vel augue vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia",
-  precio: 4416,
-  descuento: 43,
-  createdAt : new Date,
-  updatedAt : new Date
-},
-]
+let productos = require('../../data/productosdb.json')
+
+  productos = productos.map(productos => {
+    let e = {
+      nombreProducto: productos.nombreProducto,
+    categoriasId: productos.categoriasId,
+    disponible: productos.disponible,
+    imagen: productos.imagen,
+    descripcion: productos.descripcion,
+    precio: productos.precio,
+    descuento: productos.descuento,
+    createdAt:new Date,
+    updatedAt:new Date
+    }
+    return e
+  })
 
 module.exports = {
   async up (queryInterface, Sequelize) {
