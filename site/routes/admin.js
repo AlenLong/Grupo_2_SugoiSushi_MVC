75afc1c375a153164853ab9899c753026f6ed121
AlenLong <alenlong1@gmail.com>
2023-01-01 17:19:08 -0300

    adminCheck

--- site/routes/admin.js
+++ site/routes/admin.js
@@ -7,14 +7,14 @@ const productValidator = require('../validations/productsValidation')
 const upload = require('../middlewares/multerProducts');
 
 /* Creación de producto */
-router.get('/crearProducts',/* adminCheck */ create)
-router.post('/crearProducts',upload.single('imagen'),productValidator,/* adminCheck */store)
+router.get('/crearProducts',productValidator,adminCheck, create)
+router.post('/crearProducts',upload.single('imagen'),productValidator, adminCheck, store)
 
-router.get('/listarProducts',/* adminCheck */list)
-router.get('/history',/* adminCheck */history)
+router.get('/listarProducts', adminCheck, list)
+router.get('/history', adminCheck, history)
 /* Edición de producto */
-router.get('/editarProducts/:id',/* adminCheck */edit)
-router.put('/editarProducts/:id',upload.single('imagen'),productValidator,/* adminCheck */storeEdit)
+router.get('/editarProducts/:id', adminCheck, edit)
+router.put('/editarProducts/:id',upload.single('imagen'),productValidator, adminCheck, storeEdit)
 
 /* Eliminación de producto */
 router.delete('/destroy/:id',/* adminCheck */destroy);
